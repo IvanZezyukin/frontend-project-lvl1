a10307cbc029e71c1b89cf8bf71dcd85784f7d1c
IvanZezyukin <i.zezukin@gmail.com>
2022-06-21 15:57:02 +0300

    8th step done: brain-progression developed

---
+++ bin/brain-progression.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import dialogWithPlayer from '../src/index.js';
+import getAnswerFromUser from '../src/cli.js';
+import getRandomArithmeticProgression from '../src/getRandomArithmeticProgression.js';
+
+// Let's welcome user, ask his name and introduce the game
+console.log('Welcome to the Brain Games!');
+const name = getAnswerFromUser('Please, write your name right here: ');
+console.log(`Hello, ${name}!`);
+console.log('What number is missing in the progression?');
+
+// Let's make arrays for the game
+const questions = [];
+const results = [];
+for (let i = 0; i < 3; i += 1) {
+  const [question, result] = getRandomArithmeticProgression();
+  questions.push(question);
+  results.push(result);
+}
+
+// Let's call our game engine!
+dialogWithPlayer(questions, results, name);
---
+++ src/getRandomArithmeticProgression.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+
+const getRandomArithmeticProgression = () => {
+  let startItem = _.random(1, 14);
+  const length = _.random(5, 10);
+  const indexOfHiddenElement = _.random(1, length);
+  const progressionStep = _.random(1, 5);
+  const array = [startItem];
+  for (let i = 0; i < length; i += 1) {
+    array.push(startItem += progressionStep);
+  }
+  const result = array.splice(indexOfHiddenElement, 1, '..');
+  return [array.join(' '), result.join()];
+};
+
+export default getRandomArithmeticProgression;
